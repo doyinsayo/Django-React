d927bf066b06f9c78336347745bd0d650b65d2a1
Doyinsayo Otufadebo <doyinsa01@gmail.com>
2020-09-06 17:46:54 +0100

    authenticating users

--- front-end/gui3/src/components/Form.js
+++ front-end/gui3/src/components/Form.js
@@ -4,9 +4,15 @@ import axios from 'axios';
 
 class CustomForm extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
   handleFormSubmit = (event,requestType, articleID) => {
       const title = event.target.elements.title.value;
       const content = event.target.elements.content.value;
+
 
       switch ( requestType ){
           case 'post':
@@ -32,8 +38,8 @@ class CustomForm extends React.Component {
     return (
         <div>
         <Form onSubmit = {(event) => this.handleFormSubmit(event,
-            this.props.requestType,
-            this.props.articleID)}>
+              this.props.requestType,
+              this.props.articleID)}>
             <Form.Item label="Title">
             <Input name="title" placeholder="Put a title here" />
             </Form.Item>
@@ -41,7 +47,7 @@ class CustomForm extends React.Component {
             <Input name="content" placeholder="Enter some content ..." />
             </Form.Item>
             <Form.Item>
-            <Button type="primary" htmlType="submit" >{this.props.btnText}</Button>
+            <Button type="submit" htmlType="submit">{this.props.btnText}</Button>
             </Form.Item>
         </Form>
         </div>
